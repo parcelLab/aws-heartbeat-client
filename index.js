4238dcce940c0c93d11b46a5c79fb63d0cbe02f1
Julian Krenge <julian@parcellab.com>
2017-12-01 21:51:04 +0100

    Less aggressive validation of pulse 💉

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 */
 
 // dependencies
-const request = require("request");
-const _ = require('underscore');
+var request = require('request');
+var _ = require('underscore');
 
 ///////////////
 // Prototype //
@@ -14,16 +14,16 @@ const _ = require('underscore');
  * @param {string} baseUrl - the url of your aws api without the query parameters
  * @param {number} pulse  - seconds to wait for next api call 
  */
-function HeartbeatTimer(baseUrl, pulse) {
+function Heartbeat(baseUrl, pulse) {
 
   // validate
   if (_.isUndefined(baseUrl)) return console.error('Url of heartbeat service is missing');
-  if (!_.isString(baseUrl)) return console.error('BaseUrl must be a string');
-  if (_.isUndefined(pulse)) pulse = 0;
-  if (!_.isNumber(pulse) || _.isNaN(pulse) || pulse < 0)
-    return console.error('Pulse must be positive integer');
+  if (!_.isNumber(pulse) || _.isNaN(pulse) || pulse < 0) return console.error('Pulse must be positive integer');
+  if (_.isUndefined(pulse)) pulse = 60;
+
   // init
   this._lastPulse = {};
+
   // set
   this._pulse = pulse;
   this._baseUrl = baseUrl;
@@ -38,34 +38,25 @@ function HeartbeatTimer(baseUrl, pulse) {
  * @param {string} name
  * @param {HeartbeatTimer~callback}[callback] - optional
  */
-HeartbeatTimer.prototype.pulse = function (host, category, type, name, callback) {
+Heartbeat.prototype.pulse = function (host, category, type, name, callback) {
 
-  var beatId = `${host}-${category}-${type}-${name}`;
+  var beatId = host + '-' + category + '-' + type + '-' + name;
   var lastPulse = _.has(this._lastPulse, beatId) ? this._lastPulse[beatId] : 0;
 
   if (Date.now() - lastPulse < this._pulse * 1000) {
     typeof callback === 'function' ? callback(null, 'Skipped pulse: Last pulse was send less than ' + this._pulse + ' seconds ago') : console.log('Skipped pulse: Last pulse was send less than ' + this._pulse + ' seconds ago');
 
   } else {
-    if (_.isString(host) && _.isString(category) && _.isString(type) && _.isString(name)) {
-      this._lastPulse[beatId] = Date.now();
-      var url = `${this._baseUrl}/pulse?host=${host}&category=${category}&type=${type}&name=${name}`;
-      request(url, function (err, res, body) {
-        if (!err) {
-          typeof callback === 'function' ? callback(null, body) : console.log(body);
 
-        } else {
-          typeof callback === 'function' ? callback(err) : console.log(err);
-        }
-      });
+    this._lastPulse[beatId] = Date.now();
+    var url = this._baseUrl + '/pulse?host=' + host + '&category=' + category + '&type=' + type + '&name=' + name;
+    
+    request(url, function (err, res, body) {
+      if (!err) (typeof callback === 'function') ? callback(null, body) : console.log(body);
+      else (typeof callback === 'function') ? callback(err) : console.log(err);
+    });
 
-    } else {
-      typeof callback === 'function' ? callback('Missing parameter') : console.log('Missing parameter');
-    }
   }
 };
 
-
-
-
-module.exports = HeartbeatTimer;
+module.exports = Heartbeat;
