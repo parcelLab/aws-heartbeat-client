6445f72772daf43ad9dc56eff9cfba4f930d07ed
Svetlomir Bankov <svetlomirbankovv@gmail.com>
2022-03-28 17:53:11 +0200

    revert changes. Only keep pulse timer

--- index.js
+++ index.js
@@ -3,15 +3,8 @@
 */
 
 // dependencies
+var request = require('request');
 var _ = require('underscore');
-
-const { GraphQLClient, gql } = require('graphql-request')
-const graphQlUri = 'https://zelda.parcellab.com/graphql-next'
-const graphQLClient = new GraphQLClient(graphQlUri, {
-  headers: {
-    Authorization: 'Token ' + "2ef0cbdc1402c844ca70092826a3186c16e504a1"
-  }
-})
 
 ///////////////
 // Prototype //
@@ -22,15 +15,21 @@ const graphQLClient = new GraphQLClient(graphQlUri, {
  * @param {number} pulse  - seconds to wait for next api call 
  */
 function Heartbeat(baseUrl, pulse = 60) {
+
+  // validate
+  if (_.isUndefined(baseUrl)) return console.error('Url of heartbeat service is missing');
+  if (!_.isNumber(pulse) || _.isNaN(pulse) || pulse < 0) return console.error('Pulse must be positive integer');
+
+  // init
   this._lastPulse = {};
 
-  // Set pulse timer to 15 mins and ignore pulse and baseUrl from function call. These are only used for legacy compatibility and are otherwise unneeded
-  this._pulse = 900;
+  // set pulse to every 30 mins, way more than enough. Ignore function setting
+  this._pulse = 1800;
   this._baseUrl = baseUrl;
 }
 
 /**
- * Send a pulse to the AWS heartbeat service
+ * Send a pulse to Zelda
  * @name HeartbeatTimer#pulse
  * @param {string} host
  * @param {string} category
@@ -43,15 +42,6 @@ Heartbeat.prototype.pulse = function (host, category, type, name, threshold, cal
 
   if (typeof threshold === 'function') callback = threshold;
 
-
-  var beatId = host + '-' + category + '-' + type + '-' + name;
-  var lastPulse = _.has(this._lastPulse, beatId) ? this._lastPulse[beatId] : 0;
-  var thresholdHrs = 25
-
-  // set threshold to 0 for heartbeats, which are raised actively, instead of always pulsing
-  if (category === "manfred") {
-    thresholdHrs = 0
-  }
   // only pulse in production mode
   if (process.env.PRODUCTION === undefined
     || !(/^(?:yes|true|1|on)$/i).test(process.env.PRODUCTION.toString())) {
@@ -59,44 +49,25 @@ Heartbeat.prototype.pulse = function (host, category, type, name, threshold, cal
     if (typeof callback === 'function') callback(null, { result: 'aborted' });
     return false;
   }
+
+  var beatId = host + '-' + category + '-' + type + '-' + name;
+  var lastPulse = _.has(this._lastPulse, beatId) ? this._lastPulse[beatId] : 0;
+
   if (Date.now() - lastPulse < this._pulse * 1000) {
     if (typeof callback === 'function') callback(null, { result: 'skipped' });
 
   } else {
 
     this._lastPulse[beatId] = Date.now();
-    const query = gql`
-          mutation createHeartbeat($name: String!, $category: String!, $host: String!, $type: String!,$lastSuccessAt: DateTime!, $thresholdHrs: Int!, $status: AlertStatus!){
-            updateCreateHeartbeat(input: { hostName: $host, category: $category, type: $type, name: $name ,lastSuccessAt: $lastSuccessAt, thresholdHrs: $thresholdHrs, status: $status}) {
-          ... on LegacyHeartbeatType {
-                id
-                hostName
-                category
-                type
-                name
-                lastSuccessAt
-                thresholdHrs
-                status
-              }
-            }
-          }
-          `
-    const variables = {
-      name: name,
-      category: category,
-      host: host,
-      type: type,
-      lastSuccessAt: new Date(Date.now()),
-      thresholdHrs: thresholdHrs,
-      status: "CLOSED"
-    }
-    graphQLClient.request(query, variables)
-      .then((data) => {
-        (typeof callback === 'function') ? callback(null, { statusCode: 200, body: "Successful Heartbeat pulse" }) : console.log("Successful Heartbeat pulse")
-      })
-      .catch((err) => {
-        (typeof callback === 'function') ? callback({ statusCode: err.statusCode, body: 'error' + `${err}` }) : console.log(`${err}`)
-      })
+    var slash = /\/$/.test(this._baseUrl) ? '' : '/';
+    var url = this._baseUrl + slash + 'pulse?host=' + host + '&category=' + category + '&type=' + type + '&name=' + name;
+    if (threshold && typeof threshold !== 'function') url += '&threshold=' + threshold;
+    request(url, function (err, res, body) {
+      if (err) (typeof callback === 'function') ? callback(err) : console.error(err);
+      else if (res && res.statusCode >= 400) (typeof callback === 'function') ? callback(new Error(res.statusCode + ' ' + res.statusMessage)) : console.log(res.statusMessage);
+      else (typeof callback === 'function') ? callback(null, body) : console.log(body);
+    });
+
   }
 };
 
